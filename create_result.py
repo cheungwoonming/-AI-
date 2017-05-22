f5aa94e76dd1bd32e32277483bf6743b6e623cac
ZHM <765979416@qq.com>
2017-05-22 11:44:40 +0800

    update

--- create_result.py
+++ create_result.py
@@ -12,25 +12,25 @@ import tool
 #     su += (ne-la)
 # print(su/31)
 
-# add = 116583
-#
-# date = tool.date_range('20160901', '20160930', '%Y%m%d')
-#
-# writer = csv.writer(open('result/Tianchi_power_predict_table.csv', 'w', newline='', encoding='utf-8'))
-# writer.writerow(['predict_date', 'predict_power_consumption'])
-# reader = csv.reader(open('power_last.csv'))
-# reader = list(reader)
-# for index, i in enumerate(date):
-#     value = float(reader[index][1]) + add
-#     writer.writerow([i, str(int(value))])
+add = 116583
 
+date = tool.date_range('20160901', '20160930', '%Y%m%d')
 
 writer = csv.writer(open('result/Tianchi_power_predict_table.csv', 'w', newline='', encoding='utf-8'))
 writer.writerow(['predict_date', 'predict_power_consumption'])
-reader = csv.reader(open('Tianchi_power_predict_table.csv'))
+reader = csv.reader(open('power_last.csv'))
 reader = list(reader)
-for i in reader[1:]:
-    temp = int(i[1])
-    temp *= 1.1
-    temp = int(round(temp, 0))
-    writer.writerow([i[0], str(temp)])
+for index, i in enumerate(date):
+    value = float(reader[index][1]) + add
+    writer.writerow([i, str(int(value))])
+
+
+# writer = csv.writer(open('result/Tianchi_power_predict_table.csv', 'w', newline='', encoding='utf-8'))
+# writer.writerow(['predict_date', 'predict_power_consumption'])
+# reader = csv.reader(open('Tianchi_power_predict_table.csv'))
+# reader = list(reader)
+# for i in reader[1:]:
+#     temp = int(i[1])
+#     temp *= 1.1
+#     temp = int(round(temp, 0))
+#     writer.writerow([i[0], str(temp)])
